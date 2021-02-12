d2ec30cef8173e871a81fddc368d81133a56869a
seilylook@naver.com <seilylook@naver.com>
2021-02-12 22:26:39 +0900

    17 commit

---
+++ src/main/java/Registration/registration/web/MemberController.java
@@ -0,0 +1,20 @@
+package Registration.registration.web;
+
+import Registration.registration.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+}
