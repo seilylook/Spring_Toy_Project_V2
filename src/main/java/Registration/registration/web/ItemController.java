ab11c7ede985b32c092d9a453460117d6f4e55bc
seilylook@naver.com <seilylook@naver.com>
2021-02-16 23:22:47 +0900

    24 commit

---
+++ src/main/java/Registration/registration/web/ItemController.java
@@ -0,0 +1,19 @@
+package Registration.registration.web;
+
+import Registration.registration.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping(value = "/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+}
